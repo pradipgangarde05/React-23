23900c1f4e40781d2f7cdd0fc88465f4fbc84ea9
Pradip Gangarde <pradipgangarde05@gmail.com>
2023-06-13 09:21:21 +0530

    unused-fun

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App(){
-  return "hello  react "
-
+// JSX = JS + HTML
+// IF A FUNCTION RETURNS HTML, THIS BECOMES A USER DEFINED TAG
+// <App></App>
+// <App />
+// Compare with PreDefineTag
+// h1, p, li,
+// Why Master Case Convention ?
+// For Pre Define Tag  :: Lowercase :: <h1>
+// For User Define Tag :: MasterCase :: <App>
+function App() {
+  return <h1>Hello react day second</h1>;
 }
+
+// UNUSED!!!
+// JSX => JS + HTML/XML
+// IF your function returns HTML
+// Convention:: Start your function name in captial letter.
+function Sum() {
+  return <h1>100</h1>;
+}
+
 export default App;
