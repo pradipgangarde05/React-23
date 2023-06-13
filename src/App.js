64f3eac25b1202e1124469dfcdb4167ab189173b
Pradip Gangarde <pradipgangarde05@gmail.com>
2023-06-13 17:19:36 +0530

    import-datablock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Data from  "./Data";
 function App() {
   return (
     <>
-      <h1 className="bg-primary text-light p-3">My Project</h1>
+      <h1 className="bg-success text-light p-3">My Project</h1>
       <Data />
       <Data />
       <Data />
@@ -10,25 +11,4 @@ function App() {
     </>
   );
 }
-
-
-function Data() {
-  return (
-    <>
-      <div className="alert alert-dark">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse nihil
-        adipisci ducimus quisquam a rerum accusantium consequuntur. Quasi itaque
-        qui consectetur, culpa iste nam in quibusdam quisquam doloribus, dicta
-        perspiciatis accusamus praesentium! Quos accusantium explicabo alias
-        perspiciatis voluptates totam consectetur laboriosam voluptas cumque,
-        amet quasi vero nostrum quibusdam ad in mollitia soluta! Voluptatem,
-        porro quo ut in et corporis sunt facere accusantium soluta nisi
-        reprehenderit repellat, minus error, veniam ipsa quasi amet. Numquam hic
-        doloribus, aspernatur saepe .
-      </div>
-    </>
-  );
-}
-
 export default App;
