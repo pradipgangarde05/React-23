c4bf083a80ba77e3a1af30c8fbb6db75f3066373
Pradip Gangarde <pradipgangarde05@gmail.com>
2023-06-14 10:58:41 +0530

    update-body

--- src/App.js
+++ src/App.js
@@ -19,7 +19,23 @@ function Appfooter(){
 function Appbody(){
   return(
     <>
-    <h2>Insiside Appbody tag</h2>
+        <div className="alert alert-success fs-3">
+        <h2>Insiside Appbody tag</h2>
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        rem corrupti ipsam, necessitatibus, ut tempore nisi aspernatur cumque
+        blanditiis quae quia. Corrupti, in ducimus. Ullam suscipit inventore
+        quam ex non. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatibus tempora eum cupiditate. Nobis id, facere iure debitis eum a
+        at, nulla placeat, tenetur tempore ratione adipisci deleniti alias
+        aspernatur voluptatem! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Perspiciatis rem corrupti ipsam, necessitatibus, ut
+        tempore nisi aspernatur cumque blanditiis quae quia. Corrupti, in
+        ducimus. Ullam suscipit inventore quam ex non. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Voluptatibus tempora eum cupiditate.
+        Nobis id, facere iure debitis eum a at, nulla placeat, tenetur tempore
+        ratione adipisci deleniti alias aspernatur voluptatem!
+      </div>
     </>
   )
   }
