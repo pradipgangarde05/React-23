996602d5f85c19531ca02df9a35d8988305566a9
Pradip Gangarde <pradipgangarde05@gmail.com>
2023-06-14 13:19:20 +0530

    dislike-update

--- src/newpages/counterApp.js
+++ src/newpages/counterApp.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+
 function CounterApp(){
     let [counter,setCounter] = useState(1);
-    let id = 100;
-    let pi = 3.14;
-    let str = "Hello World";
-    let active = true;
+    let [incounter,setinCounter] = useState(1);
+    // let id = 100;
+    // let pi = 3.14;
+    // let str = "Hello World";
+    // let active = true;
   
    
   //Member Function
@@ -13,15 +15,22 @@ function CounterApp(){
       console.log("I'am button click",counter);
       setCounter(counter);
     };
-    let dislikeMeAction = () => {};
 
-
+     let dislikeMeAction = () => {
+     incounter  = incounter + 1;
+      console.log("I'am button click",incounter);
+      setinCounter(incounter);
+     };
+  
     return(
     <>
    <h1>{counter}</h1>
     <input type="button" value="Like Me" onClick={likeMeAction} />
+    <h1>{incounter}</h1>
+    <a name="" id="" class="btn btn-primary" href="#" role="button" onClick={dislikeMeAction}>Dislike</a>
+
     </>
     );
   }
-export default  CounterApp;  
+    export default  CounterApp;  
   
