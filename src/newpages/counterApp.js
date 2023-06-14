8b5e6742379f7a1215b5a1349219e7a7558abcb9
Pradip Gangarde <pradipgangarde05@gmail.com>
2023-06-15 01:09:44 +0530

    counter-ver-4

--- src/newpages/counterApp.js
+++ src/newpages/counterApp.js
@@ -1,36 +1,37 @@
 import { useState } from "react";
 
-function CounterApp(){
-    let [counter,setCounter] = useState(1);
-    let [incounter,setinCounter] = useState(1);
-    // let id = 100;
-    // let pi = 3.14;
-    // let str = "Hello World";
-    // let active = true;
-  
-   
-  //Member Function
-    let likeMeAction = () => {
-      counter  = counter + 1;
-      console.log("I'am button click",counter);
-      setCounter(counter);
-    };
-
-     let dislikeMeAction = () => {
-     incounter  = incounter + 1;
-      console.log("I'am button click",incounter);
-      setinCounter(incounter);
-     };
-  
-    return(
+function App() {
+  return (
     <>
-   <h1>{counter}</h1>
-    <input type="button" value="Like Me" onClick={likeMeAction} />
-    <h1>{incounter}</h1>
-    <a name="" id="" class="btn btn-primary" href="#" role="button" onClick={dislikeMeAction}>Dislike</a>
-
+      <h1>Counter App</h1>
+      <CounterApp />
     </>
-    );
-  }
-    export default  CounterApp;  
-  
+  );
+}
+
+function CounterApp() {
+  // let counter = 100; // stateless
+  let [counter, setCounter] = useState(100); // stateful + DOM Opr
+
+  let likeMeAction = () => {
+    counter = counter + 1;
+    console.log(counter);
+
+    // Asking react to perform dom opr
+    // how to trigger the DOM Opr
+    setCounter(counter);
+  };
+
+  return (
+    <>
+      <h1>{counter}</h1>
+      <span>{counter}</span>
+      <div>{counter}</div>
+      <h1>{counter}</h1>
+      <h1>{counter}</h1>
+      <input type="button" value="Like Me" onClick={likeMeAction} />
+    </>
+  );
+}
+
+export default App;
