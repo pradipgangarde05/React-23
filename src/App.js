34b8e1807ca1b80a96eb4663e25d6cd25fefb936
Pradip Gangarde <pradipgangarde05@gmail.com>
2023-06-14 11:56:03 +0530

    counter-fun

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Appbody from "./newpages/Appbody";
 import Appfooter from "./newpages/Appfooter";
 import AppHeader from "./newpages/Appheader";
@@ -6,13 +7,34 @@ import AppHeader from "./newpages/Appheader";
 function App(){
   return ( <>
  <h1>Hello app function</h1>
-<AppHeader/>
-<Appbody/>
-<Appfooter/>
+        < CounterApp/>
   </>
   )
 }
 
-  
+function CounterApp(){
+  let [counter,setCounter] = useState(1);
+  let id = 100;
+  let pi = 3.14;
+  let str = "Hello World";
+  let active = true;
+
+ 
+//Member Function
+  let likeMeAction = () => {
+    counter  = counter + 1;
+    console.log("I'am button click",counter);
+    setCounter(counter);
+  };
+  let dislikeMeAction = () => {};
+
+  return(
+  <>
+ <h1>{counter}</h1>
+  <input type="button" value="Like Me" onClick={likeMeAction} />
+  </>
+  );
+}
+
 
 export default App;
