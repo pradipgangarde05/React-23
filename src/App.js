27a2abaf4d709bed7a3013986396abb2d590980f
Pradip Gangarde <pradipgangarde05@gmail.com>
2023-06-14 09:11:41 +0530

    multiple function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+//app is user defined tag
+function App(){
+  return ( <>
+ <h1>Hello app function</h1>
+<Header/>
+<Appbody/>
+<Appfooter/>
+  </>
+  )
+}
+function Appfooter(){
+  return(
+    <>
+    <h2>Insiside footer tag</h2>
+    </>
+  )
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Appbody(){
+  return(
+    <>
+    <h2>Insiside Appbody tag</h2>
+    </>
+  )
+  }
+  
+function Header(){
+return(
+  <>
+  <h1>Inside header tag</h1>
+  </>
+)
 }
 
 export default App;
